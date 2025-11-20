e5ee6e5c219cb034e85b7d0b5ba292895d486242
wonjung <wonjung1919@gmail.com>
2025-11-20 23:33:45 +0900

    feat: BoardVO 생성

---
+++ src/main/java/com/example/javacrud4/vo/BoardVO.java
@@ -0,0 +1,55 @@
+package com.example.javacrud4.vo;
+
+import java.sql.Timestamp;
+
+public class BoardVO {
+    private int id;
+    private String title;
+    private String writer;
+    private String content;
+    private Timestamp regdate;
+    private int cnt;
+
+    // getter/setter
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getRegdate() {
+        return regdate;
+    }
+    public void setRegdate(Timestamp regdate) {
+        this.regdate = regdate;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+}
