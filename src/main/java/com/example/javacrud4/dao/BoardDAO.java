c9c521201933582be24acc89e7b8fd30efed8e25
wonjung <wonjung1919@gmail.com>
2025-11-21 10:27:39 +0900

    feat: dao에 insert 추가

---
+++ src/main/java/com/example/javacrud4/dao/BoardDAO.java
@@ -0,0 +1,70 @@
+package com.example.javacrud4.dao;
+
+import com.example.javacrud4.util.JDBCUtil;
+import com.example.javacrud4.vo.BoardVO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+// DB에 접근하고, 데이터를 다루는 모든 코드(조회, 추가, 수정, 삭제)
+public class BoardDAO {
+    private final String Board_LIST = "select * from Board order by id ASC"; // 오름차순 정렬로 들고오기
+    private final String Board_INSERT = "INSERT INTO Board (title, writer, content) values (?,?,?)";
+    private final String Board_UPDATE = "";
+    private final String Board_DELETE = "";
+    private final String Board_Get = "";
+
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    // 데이터 추가하기
+    public int insertList(BoardVO vo){
+        try{
+            conn = JDBCUtil.getConnection();
+            pstmt = conn.prepareStatement(Board_INSERT);
+            pstmt.setString(1, vo.getTitle());
+            pstmt.setString(2, vo.getWriter());
+            pstmt.setString(3, vo.getContent());
+            pstmt.executeUpdate(); // 결과 저장
+
+            return 1;
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return 0;  // 실패한 경우 return 0으로 cnt 증가 안하도록
+    }
+
+    // 전체 목록 조회하기
+    public List<BoardVO> getList(){
+        List<BoardVO> list = new ArrayList<BoardVO>();
+
+        // 연결, sql 준비 및 실행 (조회)
+        try(Connection conn = JDBCUtil.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(Board_LIST);
+            ResultSet rs = pstmt.executeQuery()){
+
+                // DB에서 데이터 들고와서 VO에 넣기
+                while(rs.next()){
+                    BoardVO info = new BoardVO();
+                    info.setId(rs.getInt("id"));
+                    info.setTitle(rs.getString("title"));
+                    info.setWriter(rs.getString("writer"));
+                    info.setContent(rs.getString("content"));
+                    info.setRegdate(rs.getTimestamp("regdate"));
+                    info.setCnt(rs.getInt("cnt"));
+                    list.add(info); // 데이터 저장
+                }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
--- src/main/java/com/example/javacrud4/util/test.java
+++ src/main/java/com/example/javacrud4/util/test.java
@@ -1,6 +1,10 @@
 package com.example.javacrud4.util;
 
+import com.example.javacrud4.dao.BoardDAO;
+import com.example.javacrud4.vo.BoardVO;
+
 import java.sql.Connection;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
@@ -11,6 +15,14 @@ public class test {
             System.out.println("DB 연결 테스트 성공!");
         } else {
             System.out.println("DB 연결 테스트 실패...");
+        }
+
+        // 리스트 조회
+        BoardDAO dao = new BoardDAO();
+        List<BoardVO> list = dao.getList();
+
+        for (BoardVO vo : list) {
+            System.out.println(vo.getId() + " / " + vo.getTitle());
         }
     }
 }
